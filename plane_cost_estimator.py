daae3af3d07d85669e312d792cb82dacf502796f
Kieli Murray <kiemurray@ucdavis.edu>
2026-01-18 11:32:41 -0800

    updated cost code to estimate RDTE and unit cost appropriately

--- plane_cost_estimator.py
+++ plane_cost_estimator.py
@@ -10,64 +10,62 @@ Tmax = 41000             #Engine max thrust lbs
 Tturbine_inlet = 4060    #Turbine inlet temperature Rankine 
 Neng = 2                 #Number of engines per aircraft
 
-#Labor and CPI
-RE = 59.10  #Engineering rate
-RT = 60.7   #Tooling rate
-RM = 50.1   #Manufacturing rate
-RQC = 55.4  #QC rate
-CPI_1986_to_2026 = 3.0 #Come back w real CPI or change to current labor rates (pick whatever is cheaper)
+#Labor costs in 2026$ and CPI value (labor cost using graph from slides)
+year = 2026
+RE = 2.576*year - 5058    #Engineering rate
+RT = 2.883*year - 5666    #Tooling rate
+RM = 2.316*year - 4552    #Manufacturing rate
+RQC = 2.6*year - 5112     #QC rate
+print(f"\nRE: {RE:.2f} $/hr")
+print(f"RT: {RT:.2f} $/hr")
+print(f"RM: {RM:.2f} $/hr")
+print(f"RQC:{RQC:.2f} $/hr")
+CPI_1986_to_2026 = 2.94
 
 #Estimated avionics cost 
-Cavionics = 40e6   #current money, same as superhornet
+Cavionics = 40e6   #current money
 
-#Engineering hours 
-HE = 4.86 * We**0.777 * Vmax**0.894 * Q**0.163
-print(f"Engineering hours (HE): {HE:.1f} hrs")
+#Man Hours
+HE = 4.86 * We**0.777 * Vmax**0.894 * Q**0.163 #Engineering hours 
+HT = 5.99 * We**0.777 * Vmax**0.696 * Q**0.263 #Tooling hours
+HM = 7.37 * We**0.82 * Vmax**0.484 * Q**0.641  #Manufacturing hours
+HQC = 0.133 * HM                               #QC hours
+print(f"\nEngineering hours:   {HE:.1f} hrs")
+print(f"Tooling hours:       {HT:.1f} hrs")
+print(f"Manufacturing hours: {HM:.1f} hrs")
+print(f"QC hours:            {HQC:.1f} hrs")
 
-#Tooling hours
-HT = 5.99 * We**0.777 * Vmax**0.696 * Q**0.263
-print(f"Tooling hours (HT): {HT:.1f} hrs")
-
-#Manufacturing hours
-HM = 7.37 * We**0.82 * Vmax**0.484 * Q**0.641
-print(f"Manufacturing hours (HM): {HM:.1f} hrs")
-
-#QC hours
-HQC = 0.133 * HM
-print(f"QC hours (HO): {HQC:.1f} hrs")
-
-#Development support cost 
-CD = (45.42 * We**0.63 * Vmax**1.3) * CPI_1986_to_2026
-print(f"Development support cost: ${CD/1e6:.2f} million")
-
-#Flight test cost 
-CF = (1243.03 * We**0.325 * Vmax**0.822 * FTA**1.21) * CPI_1986_to_2026
-print(f"Flight test cost: ${CF/1e6:.2f} million")
-
-#Manufacturing materials cost
-CM = (11.0 * We**0.921 * Vmax**0.621 * Q**0.799) * CPI_1986_to_2026
-print(f"Manufacturing materials cost: ${CM/1e6:.2f} million")
+#Labor costs
+C_eng_hours = HE * RE 
+C_tool_hours = HT * RT 
+C_mfg_hours  = HM * RM 
+C_QC_hours   = HQC * RM 
+C_labor_tot = C_eng_hours + C_tool_hours + C_mfg_hours + C_QC_hours
+print(f"\nTotal labor cost: ${C_labor_tot/1e9:.2f} billion")
 
 #Engine production cost 
 Ceng = 1548 * (0.043*Tmax + 243.25*Mmax + 0.969*Tturbine_inlet - 2228) * CPI_1986_to_2026 #should be around 20 million
-print(f"Engine production cost per engine: ${Ceng/1e6:.2f} million")
 Ceng_total = Ceng * Neng
-print(f"Total engine cost per aircraft: ${Ceng_total/1e6:.2f} million")
+print(f"\nEngine production cost per engine: ${Ceng/1e6:.2f} million")
+print(f"Total engine cost per aircraft:    ${Ceng_total/1e6:.2f} million")
 
-#Labor costs
-C_eng_hours = HE * RE * CPI_1986_to_2026
-C_tool_hours = HT * RT * CPI_1986_to_2026
-C_mfg_hours  = HM * RM * CPI_1986_to_2026
-C_QC_hours   = HQC * RM * CPI_1986_to_2026
-C_labor_tot = C_eng_hours + C_tool_hours + C_mfg_hours + C_QC_hours
-print(f"Total labor cost: ${C_labor_tot/1e9:.2f} billion")
+# Non-labor Costs
+CD = (45.42 * We**0.63 * Vmax**1.3) * CPI_1986_to_2026                  #Development support cost 
+CF = (1243.03 * We**0.325 * Vmax**0.822 * FTA**1.21) * CPI_1986_to_2026 #Flight test cost 
+CM = (11.0 * We**0.921 * Vmax**0.621 * Q**0.799) * CPI_1986_to_2026     #Manufacturing materials cost
+print(f"\nDevelopment support cost: ${CD/1e6:.2f} million")
+print(f"Flight test cost:         ${CF/1e6:.2f} million")
+print(f"Materials cost:           ${CM/1e6:.2f} million")
 
 #need to find RDTE and unit costs still:
 RDTE = C_eng_hours+ CF + CD + C_tool_hours
-print(f"RDT&E cost: ${RDTE/1e6:.2f} million")
-flyaway = C_mfg_hours + C_QC_hours + Ceng + Cavionics + CM
-unit = (RDTE + 500*flyaway)/500 
-print(f"unit cost: $ {unit/1e6:.2f} million")
+modernization_factor = 1.5 #covers software dev, program management, other factors not covered by dapca method
+RDTE *= modernization_factor
+flyaway_unit = C_mfg_hours/Q + C_QC_hours/Q + Ceng + Cavionics + CM/Q
+unit = (RDTE + 500*flyaway_unit)/500 
+print(f"\nRDT&E cost:   ${RDTE/1e9:.2f} billion")
+print(f"Flyaway cost: ${flyaway_unit/1e6:.2f} million/unit")
+print(f"Unit cost:    ${unit/1e6:.2f} million")
 
 
 
