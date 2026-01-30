5b9ffc4f196d7bcf6e5af69a6addb5a14875b284
Kieli Murray <kiemurray@ucdavis.edu>
2026-01-30 07:46:31 -0800

    added densities and speeds of sound for different altitudes

--- design_space.py
+++ design_space.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 #add ideal and necessary dashes and manuevers to see
 
 #constants (come back)
-rho_sl = 0.0     # slug/ft^3 (sea level)
 g = 32.174        # ft/s^2
 ks = 0.0            # idk how to find this 
 CD0 = 0.0          # use OpenVSP
@@ -13,14 +12,25 @@ AR = 0.0              # use OpenVSP
 e = 0.0             # ???
 k = 1 / (np.pi * AR * e)
 
+rho_40 = 0.000585189         # slug/ft^3 (cruise 40k ft)
+a_40 = 968.076               # ft/s
+rho_30 = 0.000889378
+a_30 = 994.664
+rho_20 = 0.00126659
+a_20 = 1036.85
+rho_sl = 0.00237717          
+a_SL = 1116.45
+rho_to = 0.00224392          # slug/ft^3 (sea level but 89.8F)
+
+
+
 # Wing loading range
 W_S = np.linspace(20, 200, 500)  #lbf/ft^2
 
 # Takeoff constraint
-TOFL = 0.0               # takeoff field length (ft)
-rho_to = 0.0          # slug/ft^3 (sea level but hotter)
+s_to = 0.0               # effective take off distance (ft)
 CLmax_TO = 0.0
-T_W_takeoff = ks**2 * W_S / ((rho_to/rho_sl) * g * CLmax_TO * TOFL)
+T_W_takeoff = ks**2 * W_S / (rho_to * g * CLmax_TO * s_to)
 
 # Catapult Constraints
 #
@@ -39,19 +49,20 @@ def cr_dash_constraint(v, rho, wf, T_dash_ratio):
     q = 0.5 * rho * v**2
     return (q * CD0) / (wf * T_dash_ratio * W_S) + (k * T_dash_ratio * W_S) / (wf * q)
 
-v_cr = 0.0         # ft/s Ma 0.8-0.85 at 40,000ft
-rho_cr = 0.0      # slug/ft^3 (cruise 40k ft)
-cr_wf = 0.0       # cruise weight fraction (find from weight code later, figure out if we need cruise 1 or cruise 2)
-Tcr_Tto = 0.0     # cruise thrust / take off thrust?
-T_W_cruise = cr_dash_constraint(v_cr, rho_cr, cr_wf, Tcr_Tto)
+mach_cruise = 0.85
+v_cr = mach_cruise * a_40 # ft/s Ma 0.8-0.85 at 40,000ft
+cr_wf = 0.0                  # cruise weight fraction (find from weight code later, figure out if we need cruise 1 or cruise 2)
+Tcr_Tto = 0.0                # cruise thrust / take off thrust?
+T_W_cruise = cr_dash_constraint(v_cr, rho_40, cr_wf, Tcr_Tto)
 
-v_dashSL = 0.0       # Ma 0.85-0.9 at SL
-mid_wf = 0.0         # use weight code
+mach_dashSL = 0.85
+v_dashSL = mach_dashSL * a_SL       # Ma 0.85-0.9 at SL
+mid_wf = 0.0                           # use weight code
 Tdashsl_Tto = 0.0 
 T_W_dashSL = cr_dash_constraint(v_dashSL, rho_sl, mid_wf, Tdashsl_Tto)
 
-v_dash30 = 0.0         # ft/s Ma 0.8-0.85 at 40,000ft
-rho_30 = 0.0          # slug/ft^3 (cruise 40k ft)
+mach_dash30 = 1.6     # 1.6-2.0 at 30kft
+v_dash30 = mach_dash30 * a_30          
 dash30_wf = 0.0       # weight fraction (find from weight code later, figure out what part of mission)
 Tdash30_Tto = 0.0     
 T_W_dashSL = cr_dash_constraint(v_dash30, rho_30, dash30_wf, Tdash30_Tto)
@@ -64,7 +75,6 @@ T_W_ceiling = 0.0 #add eq
 psi = 0.0              # rad/s (8.0-10.0 deg/sec at 20,000 ft mid mission fuel weight)
 v_maneuver = 0.0       # idk yet
 T20_Tto = 0.0         # midmission thrust / take off thrust
-rho_20 = 0.0
 man_wf = mid_wf
 def manuever_constraint (v, rho, wf, T_man_ratio, psi):
     q = 0.5 * rho * v**2
