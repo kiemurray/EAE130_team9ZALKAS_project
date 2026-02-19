373499efb9ffb01582dd4525020ad1a5b4de468f
Lo Chappell <llchappell@ucdavis.edu>
2026-02-18 17:01:10 -0800

    code variable declaration file

---
+++ code_variables.py
@@ -0,0 +1,2 @@
+# this is for declaring variables that are used across multiple files, such as the T_S_iteration.py and design_space.py files. This way, we can avoid hardcoding values in multiple places and maintain consistency across our codebase.
+
