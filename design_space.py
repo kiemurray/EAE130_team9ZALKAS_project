80f6d18a42d6f5c5e9e3cac7c20a46eccec5aa7d
Kieli Murray <kiemurray@ucdavis.edu>
2026-04-01 16:18:02 -0700

    added load factor lines but idt its working

--- design_space.py
+++ design_space.py
@@ -208,7 +208,7 @@ plt.plot(W_S, T_W_dash30ideal, color='limegreen', linestyle='--', linewidth=1.8,
 plt.plot(W_S, T_W_maneuver, color='red', linewidth=2, label='Maneuver (8 deg/s)')
 plt.plot(W_S, T_W_maneuver_ideal, color='red', linestyle='--', linewidth=2.2, label='Maneuver Ideal (10 deg/s)')
 plt.plot(W_S, T_W_maneuver_ideal, color='red', linestyle='--', linewidth=2.2, label='Maneuver Ideal (10 deg/s)')
-diff = np.abs(T_W_dash30ideal - T_W_maneuver_ideal)
+#diff = np.abs(T_W_dash30ideal - T_W_maneuver_ideal)
 plt.plot( (56411.39/675), (22000*2/56411.39), marker='*', color='gold', markersize=15,  markeredgecolor='black', zorder=5, label='Design Point')
 plt.axvline(W_S_stall, color='purple', linewidth=2, label='Stall')
 plt.plot(W_S, T_W_ceiling, color='darkgreen', linewidth=2, label='Service Ceiling (50,000 ft)')
