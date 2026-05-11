f1b4dff2716e133a79af41e6ecb0b32f9b4e327d
Zak Conte <zakconte@gmail.com>
2026-05-11 00:03:42 -0700

    changed MAC and forward/aft limits for raymer rule of thumb

--- Class_II_Weights.py
+++ Class_II_Weights.py
@@ -360,7 +360,7 @@ def calculate_cg(ordnance_cg, ordnance_w, label,
     return cg, denominator
 
 MAC_LE = 14.9   # MAC x location
-MAC_len = 15.0 # MAC length
+MAC_len = 17.94 # MAC length
 
 def ft_to_pct_MAC(cg_ft):
     return ((cg_ft - MAC_LE) / MAC_len) * 100
@@ -430,8 +430,8 @@ all_labels  = [l for _, l in air_handles] + [l for _, l in str_handles]
 ax.legend(all_handles, all_labels, loc='upper right', fontsize=9, ncol=2)
 
 takeoff_cg_mac = air_cg_mac[0]
-fwd_limit_mac = takeoff_cg_mac - (0.08 * 100)
-aft_limit_mac = takeoff_cg_mac + (0.08 * 100)
+fwd_limit_mac = takeoff_cg_mac - (0.04 * 100)
+aft_limit_mac = takeoff_cg_mac + (0.04 * 100)
 
 ax.axvline(x=fwd_limit_mac, color='black', linewidth=2, linestyle='-')
 ax.axvline(x=aft_limit_mac, color='black', linewidth=2, linestyle='-')
