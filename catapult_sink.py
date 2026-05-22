2959ca46ab86e45046a9c7fe4dadabea79bd7120
Kieli Murray <kiemurray@ucdavis.edu>
2026-05-22 13:10:52 -0700

    dont remember

--- catapult_sink.py
+++ catapult_sink.py
@@ -6,7 +6,13 @@ H = 10
 l = 32 
 rho = cv.rho_to 
 S = cv.S_w 
-CL = 0.7 * cv.CLmax_TO 
+CL = cv.CLmax_TO 
 m = cv.W_TO / g 
 V_min = np.sqrt(g / ((2*H / l**2) + (rho * S * CL / (2*m))))
 print(V_min)
+
+
+
+v = np.sqrt(2*cv.W_TO/(rho*S*CL))
+v = cv.ft_s_to_knots(v)
+print(v)
