bb40736b420fb0ec096da00174757fde5d0a5e4d
Zak Conte <zakconte@gmail.com>
2026-05-25 23:53:11 -0700

    new drag polar

--- refined-drag-polars.py
+++ refined-drag-polars.py
@@ -95,7 +95,7 @@ CDi_gearup_TO = [
     0.003327538, 0.00454283, 0.006043964, 0.008081831, 0.010077741, 0.012318008, 0.015374058, 0.018692723, 0.021651448, 0.02587149, 
     0.030703803, 0.035183184, 0.039177056, 0.045261898, 0.050434782, 0.056235799, 0.064198979, 0.067065006, 0.079581511, 0.082745655
 ]
-CDo_gearup = [
+CDo_gearup_TO = [
     0.004758272, 0.004783347, 0.004817332, 0.004859542, 0.004911674, 0.004971196, 0.005036944, 0.005120213, 0.005198064, 0.005297366, 
     0.005392845, 0.005508461, 0.005645136, 0.005774402, 0.005886837, 0.006033163, 0.006174594, 0.006294836, 0.00648922, 0.006645392
 ]
@@ -121,60 +121,55 @@ CDo_gearup_landing = [
 
 
 CLtot_geardown_TO = [
-    0.082789269, 0.108040001, 0.13328844, 0.158196507, 0.184358162, 0.210417072, 0.235470898, 0.260850016, 0.285702691, 0.313340893, 
-    0.340318233, 0.369617482, 0.393920027, 0.420883526, 0.441494259, 0.463048402, 0.497348487, 0.516151923, 0.550667955, 0.570237441
+    -0.137335, -0.094640, -0.043827, -0.000766, 0.042892, 0.089929, 0.136780, 0.190322, 0.237260, 0.268258,
+    0.340237, 0.398921, 0.460785, 0.545264, 0.602819, 0.616330, 0.668689, 0.795736, 0.817504, 0.882062, 0.944401
 ]
 CDtot_geardown_TO = [
-    0.01616435464, 0.01740472164, 0.01893984164, 0.02101991764, 0.02306795964, 0.02536774864, 0.02848954564, 0.03189148064, 0.03492805664, 0.03924740064,
-    0.04417519264, 0.04877018964, 0.05290073664, 0.05911484464, 0.06440016364, 0.07034750664, 0.07845211764, 0.08143838664, 0.09414927564, 0.09746959264
+    -0.108968, -0.083097, -0.066661, -0.060443, -0.060162, -0.044793, -0.042790, -0.047396, -0.042010, -0.089063,
+    -0.060920, -0.067613, -0.085049, -0.111498, -0.134947, -0.145459, -0.198479, -0.366035, -0.226011, -0.271325, -0.296964
 ]
 CDi_geardown_TO = [
-    0.01140608264, 0.01262137464, 0.01412250864, 0.01616037564, 0.01815628564, 0.02039655264, 0.02345260264, 0.02677126764, 0.02972999264, 0.03395003464,
-    0.03878234764, 0.04326172864, 0.04725560064, 0.05334044264, 0.05851332664, 0.06431434364, 0.07227752364, 0.07514355064, 0.08766005564, 0.09082419964
+    0.005771, 0.003121, 0.001705, 0.001352, 0.002013, 0.003560, 0.006323, 0.010275, 0.015565, 0.020824,
+    0.027777, 0.035907, 0.045007, 0.054300, 0.067092, 0.070960, 0.082241, 0.102820, 0.113966, 0.128859, 0.128905
 ]
-CDo_geardown = [
-    0.01283681664, 0.01286189164, 0.01289587664, 0.01293808664, 0.01299021864, 0.01304974064, 0.01311548864, 0.01319875764, 0.01327660864, 0.01337591064,
-    0.01347138964, 0.01358700564, 0.01372368064, 0.01385294664, 0.01396538164, 0.01411170764, 0.01425313864, 0.01437338064, 0.01456776464, 0.01472393664
+CDo_geardown_TO = [
+    0.004456, 0.004382, 0.004327, 0.004301, 0.004300, 0.004329, 0.004385, 0.004473, 0.004586, 0.004704,
+    0.004865, 0.005064, 0.005289, 0.005528, 0.005796, 0.006084, 0.006423, 0.006837, 0.007130, 0.007513, 0.007932
 ]
 
-CLtot_geardown_landing = [
-    0.049113681, 0.081255422, 0.113024368, 0.145261373, 0.176918958, 0.211494736,
-    0.241757704, 0.275208681, 0.310700094, 0.342560593, 0.37628135, 0.414789751
-]
-CDtot_geardown_landing = [
-    0.01486562764, 0.01597222164, 0.01752220564, 0.01965201064, 0.02218870564, 0.02533393964,
-    0.02892199164, 0.03349339764, 0.03887918064, 0.04442167964, 0.04939545664, 0.05806006364
-]
-CDi_geardown_landing = [
-    0.01013621564, 0.01122285464, 0.01274017664, 0.01482264164, 0.01729857564, 0.02036749864,
-    0.02387263364, 0.02833661964, 0.03359783464, 0.03901682264, 0.04385357864, 0.05234753464
-]
-CDo_geardown_landing = [
-    0.01280795764, 0.01282791164, 0.01286057364, 0.01290791364, 0.01296867464, 0.01304498564,
-    0.01312790264, 0.01323532264, 0.01335989064, 0.01348340164, 0.01362042164, 0.01379107364
-]
+CLtot_geardown_landing = [-0.402987, -0.341531, -0.288781, -0.235995, -0.188307, -0.131238, -0.094495, -0.045588, -0.001729, 0.044676, 0.086325, 0.134405, 0.186979, 0.236111, 0.269299, 0.347711, 0.400831, 0.476003, 0.529160, 0.599375, 0.624001, 0.665378,
+                           0.742095, 0.810511, 0.881016, 0.947732, 0.996759, 1.047330, 1.091512, 1.182781, 1.346450]
+
+CDtot_geardown_landing = [-0.268247, -0.226617, -0.191268, -0.159650, -0.130238, -0.122370, -0.081735, -0.063161, -0.055995, -0.043953, -0.048040, -0.039495, -0.040300, -0.052457, -0.057461, -0.082253, -0.112878, -0.243210, -0.111315, -0.123476, -0.163037,
+                           -0.217565, -0.206554, -0.210918, -0.278811, -0.284482, -0.420243, -0.476690, -0.515488, -0.591930, -0.609684]
+
+CDi_geardown_landing = [0.036474, 0.027603, 0.020643, 0.014586, 0.009718, 0.005856, 0.003733, 0.001687, 0.001273, 0.001930, 0.003609, 0.006253, 0.010289, 0.014318, 0.019720, 0.026495, 0.035574, 0.042728, 0.053379, 0.065971, 0.070012, 0.079994, 0.106265,
+                         0.112809, 0.127665, 0.120560, 0.154824, 0.173314, 0.192890, 0.214245, 0.240562]
+
+CDo_geardown_landing = [0.005339, 0.005127, 0.004945, 0.004788, 0.004658, 0.004553, 0.004482, 0.004427, 0.004402, 0.004404, 0.004430, 0.004489, 0.004580, 0.004674, 0.004794, 0.004958, 0.005156, 0.005357, 0.005599, 0.005893, 0.006173, 0.006503, 0.006948,
+                         0.007208, 0.007587, 0.008010, 0.008438, 0.008870, 0.009321, 0.009806, 0.010298]
 
 
 CL_clean = np.array(CLtot_clean)
-CD_clean = np.array(CD0_clean) + 0.075*(np.abs(np.array(CDi_clean))) 
-CD_gear_up_TO = np.abs(np.array(CDtot_gearup_TO)) + 0.075*(np.abs(np.array(CDi_gearup_TO)))
+CD_clean = np.array(CD0_clean) + np.array(CDi_clean)
+CD_gear_up_TO = np.array(CDo_gearup_TO) + np.array(CDi_gearup_TO)
 CL_gear_up_TO = np.array(CLtot_gearup_TO)
 CL_landing_gearup = np.array(CLtot_gearup_landing)
-CD_landing_gearup = np.abs(np.array(CDtot_gearup_landing)) + 0.075*(np.abs((np.array(CDi_gearup_landing))))
+CD_landing_gearup = np.abs(np.array(CDo_gearup_landing)) + np.array(CDi_gearup_landing)
 CL_gear_down_TO = np.array(CLtot_geardown_TO)
-CD_gear_down_TO = np.abs(np.array(CDtot_geardown_TO)) + 0.075*(np.array(CDi_geardown_TO))
+CD_gear_down_TO = np.abs(np.array(CDo_geardown_TO)) + np.array(CDi_geardown_TO)
 CL_landing_geardown = np.array(CLtot_geardown_landing)
-CD_landing_geardown = np.abs(np.array(CDtot_geardown_landing)) + 0.075*(np.abs((np.array(CDi_geardown_landing)))) 
+CD_landing_geardown = np.abs(np.array(CDo_geardown_landing)) + np.array(CDi_geardown_landing) 
 def calc_CD(CL,CL_min, CD0,e,AR,CDi):
-    CD = CD0 + (1/((3.14)*(e)*(AR)))*np.square(CL-CL_min) + 0.075*(np.abs(CDi))
+    CD = CD0 + (1/((3.14)*(e)*(AR)))*np.square(CL-CL_min) + CDi
     return CD
 e_cl = 0.71
 e_tk = 0.63 
 e_lnd = 0.50
 
 # CD_clean = calc_CD(CL_clean, min(CL_clean), CD0_clean, e_cl, cv.AR_w, CDi_clean) + CD_friction
-CD_gear_down_TO = calc_CD(CL_gear_down_TO, min(CL_gear_down_TO), CDo_geardown, e_tk, cv.AR_w, CDi_geardown_TO) + CD_friction
-CD_gear_up_TO = calc_CD(CL_gear_up_TO, min(CL_gear_up_TO), CDo_gearup, e_tk, cv.AR_w, CDi_gearup_TO) + CD_friction
+CD_gear_down_TO = calc_CD(CL_gear_down_TO, min(CL_gear_down_TO), CDo_geardown_TO, e_tk, cv.AR_w, CDi_geardown_TO) + CD_friction
+CD_gear_up_TO = calc_CD(CL_gear_up_TO, min(CL_gear_up_TO), CDo_gearup_TO, e_tk, cv.AR_w, CDi_gearup_TO) + CD_friction
 CD_landing_gearup = calc_CD(CL_landing_gearup, min(CL_landing_gearup), CDo_gearup_landing, e_lnd, cv.AR_w, CDi_gearup_landing) + CD_friction
 CD_landing_geardown = calc_CD(CL_landing_geardown, min(CL_landing_geardown), CDo_geardown_landing, e_lnd, cv.AR_w, CDi_geardown_landing) + CD_friction
 
@@ -183,10 +178,13 @@ poly_func = np.poly1d(coeffs_clean)
 x_smooth = np.linspace(-1.2, 1.36, 100) 
 y_smooth = poly_func(x_smooth)
 
+gear_down_TO_CL_offset = 0.17
+gear_down_LA_CL_offset = 0.32
+
 coeffs_gear_down_TO = np.polyfit(CL_gear_down_TO, CD_gear_down_TO, deg=2)
 poly_func_gear_down_TO = np.poly1d(coeffs_gear_down_TO) + CD_miss
 x_smooth_gear_down_TO = np.linspace(-0.9, 1.5, 100)
-y_smooth_gear_down_TO = poly_func_gear_down_TO(x_smooth_gear_down_TO) 
+y_smooth_gear_down_TO = poly_func_gear_down_TO(x_smooth_gear_down_TO)
 
 coeffs_gear_up_TO = np.polyfit(CL_gear_up_TO, CD_gear_up_TO, deg=2)
 poly_func_gear_up = np.poly1d(coeffs_gear_up_TO)
@@ -204,17 +202,17 @@ x_smooth_gear_down_LA = np.linspace(-2, 1.8, 100)
 y_smooth_gear_down_LA = poly_func_gear_down_LA(x_smooth_gear_down_LA)
 
 plt.figure(figsize=(10, 8))
-plt.plot(y_smooth, x_smooth, label="Clean", color='blue') 
-plt.plot(y_smooth_gear_down_TO, x_smooth_gear_down_TO, label="Takeoff Gear Down", color='orange')
+plt.plot(y_smooth, x_smooth, label="Clean", color='blue')
+plt.plot(y_smooth_gear_down_TO, x_smooth_gear_down_TO + gear_down_TO_CL_offset, label="Takeoff Gear Down", color='orange')
 plt.plot(y_smooth_gear_up_TO, x_smooth_gear_up_TO, label="Takeoff Gear Up", color='green')
 plt.plot(y_smooth_gear_up_LA, x_smooth_gear_up_LA, label="Landing Gear Up", color='red')
-plt.plot(y_smooth_gear_down_LA, x_smooth_gear_down_LA, label="Landing Gear Down", color='purple')
-plt.ylabel("Lift Coefficient ($C_L$)", font="Times New Roman", fontsize=12)
-plt.xlabel("Drag Coefficient ($C_D$)", font="Times New Roman", fontsize=12)
-plt.title("Drag Polar", font="Times New Roman", fontsize=14)
+plt.plot(y_smooth_gear_down_LA, x_smooth_gear_down_LA + gear_down_LA_CL_offset, label="Landing Gear Down", color='purple')
+plt.ylabel("Lift Coefficient ($C_L$)", font="Times New Roman", fontsize=18)
+plt.xlabel("Drag Coefficient ($C_D$)", font="Times New Roman", fontsize=18)
+plt.title("Drag Polar", font="Times New Roman", fontsize=18)
 plt.axhline(0, color='black', linewidth=1)
 plt.axvline(0, color='black', linewidth=1)
-plt.legend(loc='upper right', fontsize=10)
+plt.legend(loc='upper right', fontsize=18)
 plt.xlim(0, 0.2)
 plt.ylim(-1, 1.8)
 plt.grid(True)
